7b1aed1c67ca82b579bc60ca6dc51fd6020937a5
Zack Muenz <zackmuenz@gmail.com>
2018-06-09 13:20:54 -0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const App = () => (
+  <div>
+    <Nav />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Books} />
+        <Route exact path="/books" component={Books} />
+        <Route exact path="/books/:id" component={Detail} />
+        <Route component={NoMatch} />
+      </Switch>
+    </Router>
+  </div>
+);
+
+export default App;
